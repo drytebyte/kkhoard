f0c7175cecdcdc27baaadeed3de6ba6afec868b2
drytebyte <d.rytebyte@gmail.com>
2025-07-27 02:12:20 +0000

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,25 @@
+// models/Product.js
 import mongoose from 'mongoose';
 
-const ProductSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  image: String,
-}, { timestamps: true });
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    price: { type: Number, required: true },
+    image: { type: String },
+    brand: { type: String },
+    category: { type: String },
+    countInStock: { type: Number, required: true },
+    description: { type: String },
+    rating: { type: Number, default: 0 },
+    numReviews: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
+const Product =
+  mongoose.models.Product || mongoose.model('Product', productSchema);
+
+export default Product;
